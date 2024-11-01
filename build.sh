1247a3c1fde3b6b606182c621e34b9a68e91d64f
avierr <kritrim@yandex.com>
2024-11-01 21:03:03 +0000

    fix: use current version in build script without incrementing

--- build.sh
+++ build.sh
@@ -6,12 +6,6 @@ INFO_ENV_PATH="./info.env"
 # Extract the current version from info.env
 CURRENT_VERSION=$(grep -E "^current_version=" "$INFO_ENV_PATH" | cut -d'=' -f2 | tr -d '"')
 
-# Increment the patch version
-IFS='.' read -r -a version_parts <<< "$CURRENT_VERSION"
-((version_parts[2]++))
-NEW_VERSION="${version_parts[0]}.${version_parts[1]}.${version_parts[2]}"
-
-echo "Current version: $CURRENT_VERSION -> New version: $NEW_VERSION"
 
 # Build the application with the new version and respect GOOS and GOARCH
-go build -gcflags=all="-N -l" -o natsdash-${GOOS}-${GOARCH} -ldflags "-X github.com/solidpulse/natsdash/ds.Version=$NEW_VERSION"
+go build -gcflags=all="-N -l" -o natsdash-${GOOS}-${GOARCH} -ldflags "-X github.com/solidpulse/natsdash/ds.Version=$CURRENT_VERSION"
